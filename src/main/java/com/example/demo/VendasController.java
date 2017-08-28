5c80a50db9f297b826f3e75bb489fa31c6dfd2ed
grosa <grosa@CIWRKST06E0C01.cipucrs.pucrs.br>
2017-08-28 13:19:40 -0300

    testa ai

--- src/main/java/com/example/demo/VendasController.java
+++ src/main/java/com/example/demo/VendasController.java
@@ -1,6 +1,9 @@
-package com.example.demo;
+/*package com.example.demo;
 
 import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -15,12 +18,43 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Controller
 public class VendasController {
 	private ProdutoRepositorioV2 pr;
 	private VendaRepositorioV2 vr;
 	
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+    }
+}
+
 	@Autowired
 	public VendasController(ProdutoRepositorioV2 pr, VendaRepositorioV2 vr) {
 		this.pr = pr;
@@ -74,3 +108,4 @@ public class VendasController {
 	}
 	
 }
+*/
--- src/main/java/com/example/demo/VendasSpringWebJpaApplication.java
+++ src/main/java/com/example/demo/VendasSpringWebJpaApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VendasSpringWebJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VendasSpringWebJpaApplication.class, args);
-	}
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(VendasSpringWebJpaApplication.class, args);
+    }
+
 }
